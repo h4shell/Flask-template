44234af927ed7be93cad54814ee122c5ddaf4796
h4shell <60841743+h4shell@users.noreply.github.com>
2023-05-01 11:14:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask, render_template
 
 app = Flask(__name__, static_url_path='/static')
+conf = {
+  "host" : "0.0.0.0",
+  "port" : 5000
+}
 
 @app.route("/")
 def homepage():
   username = "h4shell"
   return render_template('www/index.html', user=username)
 
-
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
-
+  print(f'Server: http://{conf["host"}:{conf["port"}')
+  # app.run(host="0.0.0.0", port=5000, debug=True)
+  from waitress import serve
+  serve(app, host=conf["host"], port=conf["port"]
